122a3fa0fba9a06eae508c3324ae5eb5e066629b
scotonid <scotonid@student.ethz.ch>
2017-05-09 13:51:13 +0200

    new structure, smith predictor added, voltage oscillating

--- acceleration/variabeln.m
+++ acceleration/variabeln.m
@@ -37,7 +37,7 @@ saturation_voltage_low=0.4;
 Ta0=800;
 lambda_0=0.1;
 w0=200;
-Fz0=400*g;
+Fz0=450*g;
 
 a1= Ta0/(J*w0)-Fz0*r/(J*w0)*(theta1*theta2*exp(-lambda_0*theta2)-theta3)+Fz0*r/(J*w0)*(theta1*(1-exp(-lambda_0*theta2)-lambda_0*theta2*exp(-lambda_0*theta2))-2*lambda_0*theta3)-Fz0/(m*r*w0)*(theta1*theta2*exp(-lambda_0*theta2)-theta3);
 
@@ -52,14 +52,17 @@ A= a1;
 B= b1;
 
 
-Q=100;
+Q=1000;
 
 R=0.01;
 
 lambda_target=0.1;
 
 
-K_lqr= lqr(A,B,Q,R)
+K_lqr= lqr(A,B,Q,R);
+K_i=1000;
+
+
 
  
 max_torque=140; %Max. Drehmoment Motor
