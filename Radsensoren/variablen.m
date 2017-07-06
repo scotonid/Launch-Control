5cbb4e24abad185a537278f57c5e0ec7d83c4186
scotonid <scotonid@student.ethz.ch>
2017-07-06 22:05:08 +0200

    master

--- Radsensoren/variablen.m
+++
@@ -1,6 +0,0 @@
-upper_limit=0.8;
-lower_limit=0.1;
-counting_treshold=1;
-tooth_ratio=44/counting_treshold; %alle Zähne/gezählte Zähne
-wheel_circumference=1.83; %Radumfang
-time_exceed=20; %Zeit ohne Zahn, damit geschw. 0 wird
