c199485b1341f45d5bdbc1d7f2eef8eed621bddb
scotonid <scotonid@student.ethz.ch>
2017-07-06 21:58:27 +0200

    model based prediction

--- acceleration/variabeln.m
+++ acceleration/variabeln.m
@@ -5,10 +5,6 @@ m=700;
 g=9.81;
 r=0.25;
 J=1;
-
-% theta1=1.28; %dry asphalt
-% theta2=23.99;
-% theta3=0.52;
 theta1=0.86; %wet asphalt conditions
 theta2=33.82;
 theta3=0.35;
@@ -35,17 +31,17 @@ F_rtot=f_r*m*g; %wheel resistance of all wheels
 
 a=31.111;
 b=-12.44444; %f(x)=ax+b
-% saturation_torque_high=220;
+saturation_torque_high=220;
 % saturation_voltage_low=0;
 
 %% Matrix A, B
 
-Ta0=800;
+Ta0=1000;
 lambda_0=0.1;
 w0=200;
-Fz0=3950;
+Fz0=450*g;
 mu=0.8;
-v_pnk=4.5;
+v_pnk=3;
 
 a1= Ta0/(J*w0)-Fz0*r/(J*w0)*(theta1*theta2*exp(-lambda_0*theta2)-theta3)+Fz0*r/(J*w0)*(theta1*(1-exp(-lambda_0*theta2)-lambda_0*theta2*exp(-lambda_0*theta2))-2*lambda_0*theta3)-Fz0/(m*r*w0)*(theta1*theta2*exp(-lambda_0*theta2)-theta3);
 
@@ -55,20 +51,23 @@ b1=1/(J*w0)-lambda_0/(J*w0);
 
 %% Controller LQR
 
-Q=100;
+Q=1;
 
-R=10;
+R=0.1;
 
 lambda_target=0.1;
 
 
 K_lqr= lqr(a1,b1,Q,R);
-K_i=5;
+K_i=0.1;
 
-
+ 
 max_torque=220; %Max. Drehmoment Motor
-saturation_torque_high=max_torque*gear_ratio;
+
+
+
 
 %% 
 sim('Acceleration',5)
 Simulink.sdi.view
+
